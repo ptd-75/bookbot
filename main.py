07436bab0424c0dfbd8409c785e24fcf7ce1dece
ptd-75 <toby@ithinkican.com>
2024-11-08 17:56:27 -0600

    character counter added

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 def count_words(file_contents):
     words = file_contents.split()
     print(len(words))
-    
+
 def main():
     with open("books/frankenstein.txt") as f:
         # ...
         file_contents = f.read()
     print(file_contents)
     count_words(file_contents)
+
+    lowered_string = file_contents.lower()
+    character_counts = {}
+    for char in lowered_string:
+        if char in character_counts:
+            character_counts[char] += 1
+        else:
+            character_counts[char] = 1
+    print(character_counts)
 
 main()
